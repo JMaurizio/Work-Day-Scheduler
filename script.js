d3afc91b1263a6003de4ef2c1b0cfe149dc8f48c
JMaurizio <90101715+JMaurizio@users.noreply.github.com>
2021-11-12 15:17:52 -0600

    scheduledEvents properly storing local storage

--- script.js
+++ script.js
@@ -6,11 +6,19 @@ var scheduledEvents = [];
 var save = $(".save")
 save.on("click", function() {
     var scheduleData = {
-        time: $(".time").text(),
-        schedule: $(".schedule").val()
+        time: $(this).siblings(".time").text(),
+        schedule: $(this).siblings(".schedule").val()
     }    
     localStorage.setItem("scheduleData", JSON.stringify(scheduleData))
     scheduledEvents.push(scheduleData)
+    localStorage.setItem("scheduledEvents", JSON.stringify(scheduledEvents))
+
+    $(".schedule").each(function() {
+        if(scheduleData.time == $(this).attr("id"))
+            $(this).text(function(scheduledEvents) {
+                return scheduledEvents.schedule
+            })
+    })
 });
 
 function blockColor() {
@@ -29,6 +37,7 @@ function blockColor() {
             $(this).css("background-color","green")
             $(this).css("color","white")
         }
+
     });
 };
 
