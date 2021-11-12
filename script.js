cab37f7cdb381b84178360939bb0c165e0892375
JMaurizio <90101715+JMaurizio@users.noreply.github.com>
2021-11-12 14:15:33 -0600

    blockcolor functions working

--- script.js
+++ script.js
@@ -14,17 +14,22 @@ save.on("click", function() {
 });
 
 function blockColor() {
-    var hour = $(".planner-block").attr("id");
-    
-    if(currentTime > hour) {
-        $(this).css("background-color","red")
-    }
-    else if (currentTime == hour) {
-        $(this).css("background-color","yellow")
-    }
-    else {
-        $(this).css("background-color","green")
-    }
-}
+
+    $(".schedule").each(function() {
+    var hour = $(this).attr("id");
+        if(currentTime > hour) {
+            $(this).css("background-color","red")
+            $(this).css("color","white")
+        }
+        else if (currentTime == hour) {
+            $(this).css("background-color","yellow")
+            $(this).css("color","white")
+        }
+        else {
+            $(this).css("background-color","green")
+            $(this).css("color","white")
+        }
+    });
+};
 
 blockColor()
